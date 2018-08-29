ceaf821bab617233619e530ebbd964c374f61601
Benedict <benaq9@gmail.com>
2018-08-29 13:40:10 -0400

    cleaned up data a bit more and took another dump

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -91,7 +91,7 @@ def stat_scrape(week=1, year=2017):
                             if c.isalnum()).lower())
 
     # Parse out stats
-    stats_df = df['stats'].apply(pd.Series)
+    stats_df = df['stats'].apply(pd.Series, dtype=float)
     stats_df.fillna(0, inplace=True)
     stats_df.columns = stats_df.columns.map(int)
 
@@ -101,6 +101,10 @@ def stat_scrape(week=1, year=2017):
 
     stats_df = stats_df[[i for i in range(1,94)]]
     stats_df.columns = stat_names
+
+    # Kickoff and punt return yards and touchdowns are duplicates
+    stats_df.drop(columns='duplicate')
+
     df = pd.concat([df.drop(columns='stats'), stats_df], axis=1)
     df['week'] = np.zeros(df.shape[0], dtype=int) + week
     df['year'] = np.zeros(df.shape[0], dtype=int) + year
@@ -137,13 +141,107 @@ stat_names = []
 for stat in stats:
     stat_names.append(stat['name'])
 
+# Postgres does not like columns that begin with integers
+stat_names[31] = 'two_point_conversions'
+
+# Kickoff and punt return yards/touchdowns are duplicates
+stat_names[51] = 'duplicate'
+stat_names[52] = 'duplicate'
+
 # Clean stat_names
 stat_names = [name.replace(' ', '_') for name in stat_names]
-stat_names = [name.replace('+', '_plus') for name in stat_names]
-stat_names = [name.replace('-', '_to_') for name in stat_names]
-stat_names = [name.replace('(','') for name in stat_names]
-stat_names = [name.replace(')','') for name in stat_names]
-stat_names = [name.lower() for name in stat_names]
+stat_names = [name.replace('+', 'plus') for name in stat_names]
+stat_names = [name.replace('-', '_') for name in stat_names]
+stat_names = list(map((lambda x: ''.join(c for c in x if (c.isalnum() or c == '_')).lower()), stat_names))
+
+
+
+def query_avg(pos_string=''):
+    engine = create_engine("postgresql+psycopg2://football:isback@localhost/nfl")
+    '''
+    This function queries the database and returns a pandas DataFrame containing
+    the id, name, position, and weekly average fantasy points of the players by
+    position
+
+    PARAMETERS
+    ----------
+    pos_string: {str} to filter out certain positions, default returns all
+
+    RETURNS
+    -------
+    {pandas.DataFrame} a DataFrame containing the relevant data
+
+    '''
+    q = '''
+    SELECT id,
+        name,
+        position AS pos,
+        AVG(weekpts) AS avg_points,
+        AVG(passing_attempts) AS avg_passing_attempts,
+        AVG(passing_completions) AS avg_passing_completions,
+        AVG(incomplete_passes) AS avg_incomplete_passes,
+        AVG(passing_yards) AS avg_passing_yards,
+        AVG(passing_touchdowns) AS avg_passing_touchdowns,
+        AVG(interceptions_thrown) AS avg_interceptions_thrown,
+        AVG(every_time_sacked) AS avg_every_time_sacked,
+        AVG(rushing_attempts) AS avg_rushing_attempts,
+        AVG(rushing_yards) AS avg_rushing_yards,
+        AVG(rushing_touchdowns) AS avg_rushing_touchdowns,
+        AVG(receptions) AS avg_receptions,
+        AVG(receiving_yards) AS avg_receiving_yards,
+        AVG(receiving_touchdowns) AS avg_receiving_touchdowns,
+        AVG(kickoff_and_punt_return_yards) AS avg_kickoff_and_punt_return_yards,
+        AVG(kickoff_and_punt_return_touchdowns) AS avg_kickoff_and_punt_return_touchdowns,
+        AVG(fumble_recovered_for_td) AS avg_fumble_recovered_for_td,
+        AVG(fumbles_lost) AS avg_fumbles_lost,
+        AVG(fumble) AS avg_fumble,
+        AVG(two_point_conversions) AS avg_2_point_conversions,
+        AVG(pat_made) AS avg_pat_made,
+        AVG(pat_missed) AS avg_pat_missed,
+        AVG(fg_made_0_19) AS avg_fg_made_0_19,
+        AVG(fg_made_20_29) AS avg_fg_made_20_29,
+        AVG(fg_made_30_39) AS avg_fg_made_30_39,
+        AVG(fg_made_40_49) AS avg_fg_made_40_49,
+        AVG(fg_made_50plus) AS avg_fg_made_50plus,
+        AVG(fg_missed_0_19) AS avg_fg_missed_0_19,
+        AVG(fg_missed_20_29) AS avg_fg_missed_20_29,
+        AVG(fg_missed_30_39) AS avg_fg_missed_30_39,
+        AVG(fg_missed_40_49) AS avg_fg_missed_40_49,
+        AVG(fg_missed_50plus) AS avg_fg_missed_50plus,
+        AVG(sacks) AS avg_sacks,
+        AVG(interceptions) AS avg_interceptions,
+        AVG(fumbles_recovered) AS avg_fumbles_recovered,
+        AVG(fumbles_forced) AS avg_fumbles_forced,
+        AVG(safeties) AS avg_safeties,
+        AVG(touchdowns) AS avg_touchdowns,
+        AVG(blocked_kicks) AS avg_blocked_kicks,
+        AVG(points_allowed) AS avg_points_allowed,
+        AVG(yards_allowed) AS avg_yards_allowed,
+        AVG(tackle) AS avg_tackle,
+        AVG(assisted_tackles) AS avg_assisted_tackles,
+        AVG(sack) AS avg_sack,
+        AVG(defense_interception) AS avg_defense_interception,
+        AVG(forced_fumble) AS avg_forced_fumble,
+        AVG(fumbles_recovery) AS avg_fumbles_recovery,
+        AVG(touchdown_interception_return) AS avg_touchdown_interception_return,
+        AVG(touchdown_fumble_return) AS avg_touchdown_fumble_return,
+        AVG(touchdown_blocked_kick) AS avg_touchdown_blocked_kick,
+        AVG(blocked_kick_punt_fg_pat) AS avg_blocked_kick_punt_fg_pat,
+        AVG(safety) AS avg_safety,
+        AVG(pass_defended) AS avg_pass_defended,
+        AVG(interception_return_yards) AS avg_interception_return_yards,
+        AVG(fumble_return_yards) AS avg_fumble_return_yards,
+        AVG(qb_hit) AS avg_qb_hit,
+        AVG(sack_yards) AS avg_sack_yards,
+        AVG(def_2_point_return) AS avg_def_2_point_return,
+        AVG(team_def_2_point_return) AS avg_team_def_2_point_return
+    FROM fantasy
+    GROUP BY id, name, pos
+    {}
+    ORDER BY avg_points DESC;
+    '''.format(pos_string)
+
+    return pd.read_sql(q, engine)
 
 if __name__ == '__main__':
     for i in range(1,18):
@@ -161,6 +259,6 @@ if __name__ == '__main__':
             print('Failed to retrieve fantasy stats for Week {}'.format(i))
         else:
             print('Successfully retrieved fantasy stats for Week {}'.format(i))
-            to_database(stat_df, table_name='nflfantasy')
+            to_database(stat_df, table_name='fantasy')
 
         
