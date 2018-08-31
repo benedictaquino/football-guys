23eccd195da1e076d402d4010d91b9bffb284d34
Benedict <benaq9@gmail.com>
2018-08-31 16:40:32 -0400

    web app stuff

--- app/app.py
+++ app/app.py
@@ -4,28 +4,52 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 from flask import Flask, render_template, request, jsonify
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import plotly.plotly as py
+import json
 
-app = Flask(__name__)
+server = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+@server.route('/')
+@server.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@server.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/projects')
+@server.route('/projects')
 def projects():
     return render_template('projects.html')
 
-@app.route('/mapping-the-clutch-gene')
+@server.route('/mapping-the-clutch-gene')
 def mapping_the_clutch_gene():
     return render_template('mapping_the_clutch_gene.html')
 
-@app.route('/old_index')
+@server.route('/old_index')
 def old_index():
     return render_template('old_index.html')
+
+app = dash.Dash(__name__, server=server)
+
+with open('data/simplicial_complex.pkl', 'rb') as f:
+    simplicial_complex = go.Figure(json.load(f))
+
+app.layout = html.Div(children=[
+    dcc.Graph(
+        id='example-graph',
+        figure=simplicial_complex
+    )
+])
+
+@server.route("/qb-complex")
+def qb_complex():
+    return app.index()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    server.run(host='0.0.0.0', port=8080, debug=True)
