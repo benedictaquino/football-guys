3adf02f13a29e672e1ec1d232e11206f29713902
Benedict <benaq9@gmail.com>
2018-08-31 16:55:14 -0400

    changed port

--- app/app.py
+++ app/app.py
@@ -52,4 +52,4 @@ def qb_complex():
     return app.index()
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=8080, debug=True)
+    server.run(host='0.0.0.0', port=8000, debug=True)
