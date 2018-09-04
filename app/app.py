3110800740a65ca197a1afecbda3b553f913d33d
Benedict <benaq9@gmail.com>
2018-09-04 12:21:40 -0400

    app stuff bad commit message

--- app/app.py
+++ app/app.py
@@ -10,6 +10,8 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import plotly.plotly as py
 import json
+import numpy as np
+import pymongo
 
 server = Flask(__name__)
 # app = Flask(__name__)
@@ -37,19 +39,60 @@ def old_index():
 
 app = dash.Dash(__name__, server=server)
 
-with open('data/simplicial_complex.pkl', 'rb') as f:
-    simplicial_complex = go.Figure(json.load(f))
+positions = ['QB', 'RB', 'WR', 'TE', 'DEF', 'K', 'LB', 'DB', 'DE']
 
 app.layout = html.Div(children=[
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='position',
+                options=[{'label': pos, 'value': pos.lower()} for pos in positions],
+                value='qb'
+            ),
+            dcc.RadioItems(
+                id='complex-type',
+                options=[{'label': i, 'value': i.lower()} for i in ['Landmark', 'Observer']],
+                value='observer',
+                labelStyle={'display': 'inline-block'}
+            )
+        ])
+    ]),
     dcc.Graph(
-        id='example-graph',
-        figure=simplicial_complex
-    )
+        id='complex'
+    ),
+    html.Div([
+        dcc.Slider(
+                id='t-slider',
+                min=0.0,
+                max=10.0,
+                value=2.5,
+                step=0.5,
+                marks={str(t): str(t) for t in np.arange(0.0,10.1,0.5)}
+            )
+    ])
 ])
 
-@server.route("/qb-complex")
-def qb_complex():
+@server.route("/complex")
+def nfl_complex():
     return app.index()
 
+@app.callback(
+    dash.dependencies.Output('complex', 'figure'),
+    [dash.dependencies.Input('position', 'value'),
+     dash.dependencies.Input('complex-type', 'value'),
+     dash.dependencies.Input('t-slider', 'value')])
+def update_graph(pos, complex_type, t):
+    name = '{}_avg_{}_complex_{}'.format(pos, complex_type, float(t))
+    complex_data = COMPLEXES.find_one({'name': name})
+    del complex_data['name']
+    figure = go.Figure(complex_data)
+    return figure
+
 if __name__ == '__main__':
+    client = pymongo.MongoClient()
+    db_name = 'nfl'
+    db = client[db_name]
+    collection_name = 'complexes'
+    COMPLEXES = db[collection_name]
+
     server.run(host='0.0.0.0', port=8000, debug=True)
