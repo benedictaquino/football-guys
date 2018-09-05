eea2b269a2cb38f7c0ea00c00e44476e76d06b6f
Benedict <benaq9@gmail.com>
2018-09-05 17:41:08 -0400

    make app pretty

--- app/app.py
+++ app/app.py
@@ -130,6 +130,8 @@ def update_table(pos, t, week):
 
     return figure
 
+# Make dash pretty
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 if __name__ == '__main__':
     client = pymongo.MongoClient()
